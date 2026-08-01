b282cdeed0ac405c2890fba0aa6bb458da831dbc
Vlad Skubskyi <skubskijvlad@gmail.com>
2026-08-01 17:20:58 +0300

    feat(CV): add image preprocessing method

--- task_2_cv/src/model.py
+++ task_2_cv/src/model.py
@@ -1,9 +1,10 @@
 import torch
 import numpy as np
-import kornia as k
+import kornia as K
+import cv2
 from kornia.feature import LoFTR
 
-class SeaasonalMatcher:
+class SeasonalMatcher:
     def __init__(self, pretrained="outdoor", conf_threshold=0.2, device=None):
         if device is None:
             self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -14,3 +15,11 @@ class SeaasonalMatcher:
 
         self.matcher = LoFTR(pretrained=pretrained).to(self.device).eval()
     
+    def preprocess_image(self, img_path):
+
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        if img is None:
+            raise ValueError
+        
+        img_tensor = K.image_to_tensor(img, keepdim=False).float() / 255.0
+        return img_tensor.to(self.device)
